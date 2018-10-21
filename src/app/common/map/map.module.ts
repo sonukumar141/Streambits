776669bbb8c9e36123b11a9d083cb684e697d176
Sonu Mishra <sonukumar141@gmail.com>
2018-10-21 16:23:57 +0530

    adding display area on the map

--- src/app/common/map/map.module.ts
+++ src/app/common/map/map.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MapComponent } from './map.component';
 import { AgmCoreModule } from '@agm/core';
 import { CamelizePipe } from 'ngx-pipes';
+import { CommonModule } from '@angular/common';
 
 import { MapService } from './map.service';
 
@@ -15,7 +16,8 @@ import { MapService } from './map.service';
   imports: [
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDKhMuYssF4ls3F2idN1-NhKOf9Ov0pPl4'
-    })
+    }),
+    CommonModule
   ],
   providers: [
     MapService,
