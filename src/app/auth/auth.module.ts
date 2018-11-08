cce1dd2ff76509a753c84edc38a1de499b0b6ec7
Sonu Mishra <sonukumar141@gmail.com>
2018-11-08 11:14:56 +0530

    Adding login reactive forms

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule} from '@angular/router';
 import { CommonModule } from  '@angular/common';
  
@@ -21,7 +21,8 @@ const routes: Routes = [
   imports: [
     RouterModule.forChild(routes),
     FormsModule,
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   providers: [
   	AuthService
