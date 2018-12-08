89f9a0b1980f95925aa3ef80279e113c22e69116
Sonu Mishra <sonukumar141@gmail.com>
2018-12-08 19:58:01 +0530

    manage job bookings finish

--- src/app/manage/manage-job/manage-job-booking/manage-job-booking.component.ts
+++ src/app/manage/manage-job/manage-job-booking/manage-job-booking.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Booking } from '../../../booking/shared/booking.model';
 
 @Component({
   selector: 'streambits-manage-job-booking',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageJobBookingComponent implements OnInit {
 
-  constructor() { }
+  @Input() bookings: Booking[];
+
+  constructor(public modalService: NgbModal) { }
 
   ngOnInit() {
   }
