0c267854cc8c235eee5307a9e2eceaffac27250d
Sonu Mishra <sonukumar141@gmail.com>
2018-12-15 19:05:10 +0530

    crop image intro

--- src/app/common/components/image-upload/image-upload.component.ts
+++ src/app/common/components/image-upload/image-upload.component.ts
@@ -20,6 +20,7 @@ export class ImageUploadComponent {
   @Output() imageError = new EventEmitter();
 
   selectedFile: FileSnippet;
+  imageChangedEvent: any;
 
   constructor(private imageService: ImageUploadService) { }
 
@@ -27,7 +28,6 @@ export class ImageUploadComponent {
   	this.selectedFile.pending = false;
   	this.selectedFile.status = 'OK';
   	this.imageUploaded.emit(imageUrl);
-  	debugger;
   }
 
   private onFilure() {
@@ -36,28 +36,37 @@ export class ImageUploadComponent {
   	this.imageError.emit('');
   }
 
-  processFile(imageInput: any) {
-  	const file: File = imageInput.files[0];
-  	const reader = new FileReader();
+  imageCropped(file: File): FileSnippet | File {
+  	if(this.selectedFile) {
+  		return this.selectedFile.file = file;
+  	}
 
-  	debugger;
-  	
-  	reader.addEventListener('load', (event: any	) => {
-  		this.selectedFile = new FileSnippet(event.target.result, file);
-
-  		this.selectedFile.pending = true;
-
-  		this.imageService.uploadImage(this.selectedFile.file).subscribe(
-  			(imageUrl: string) => {
-  				this.onSuccess(imageUrl);
-  			},
-  			() => {
-  				this.onFilure();
-  			})
-
-  	});
-
-  	reader.readAsDataURL(file);
+  	return new FileSnippet('',file);
   }
 
+  processFile(event: any) {
+  	this.imageChangedEvent = event;
+  }
+
+  uploadImage(){
+  	if(this.selectedFile) {
+	  	const reader = new FileReader();
+
+	  	reader.addEventListener('load', (event: any	) => {
+
+	  		this.selectedFile.pending = true;
+
+	  		this.imageService.uploadImage(this.selectedFile.file).subscribe(
+	  			(imageUrl: string) => {
+	  				this.onSuccess(imageUrl);
+	  			},
+	  			() => {
+	  				this.onFilure();
+	  			})
+
+	  	});
+
+	reader.readAsDataURL(this.selectedFile.file);    	
+  	}
+  }
 }
--- src/app/common/components/image-upload/image-upload.module.ts
+++ src/app/common/components/image-upload/image-upload.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
+import { ImageCropperModule } from 'ngx-image-cropper';
+
 import { ImageUploadComponent } from './image-upload.component';
 
 import { ImageUploadService } from './image-upload.service';
@@ -10,7 +11,8 @@ import { ImageUploadService } from './image-upload.service';
 	
 	imports: [
 		CommonModule,
-		HttpModule
+		HttpModule,
+		ImageCropperModule
 	],
 	providers: [
 		ImageUploadService
