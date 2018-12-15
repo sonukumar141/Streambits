a2b4a72995ffdcb200a60ed64cb7bb4036b0129c
Sonu Mishra <sonukumar141@gmail.com>
2018-12-15 21:14:03 +0530

    create editable image component

---
+++ src/app/common/components/editable/editable-image/editable-image.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { EditableComponent } from '../editable-component';
+
+@Component({
+  selector: 'streambits-editable-image',
+  templateUrl: './editable-image.component.html',
+  styleUrls: ['./editable-image.component.scss']
+})
+export class EditableImageComponent extends EditableComponent {
+
+	handleImageUpload() {
+		
+	}
+
+	handleImageError() {
+
+	}
+}
